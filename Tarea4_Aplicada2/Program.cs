fd4f06126209ac78dd9eba7b6db70551c843e704
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-06-08 15:49:26 +0200

    Agregar archivos de proyecto.

---
+++ Tarea4_Aplicada2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tarea4_Aplicada2.DAL;
+using Tarea4_Aplicada2.Entidades;
+
+namespace EjemploAplicacionDeConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Contexto())
+            {
+
+                var std = new Estudiante()
+                {
+                    Name = "Bill"
+                };
+
+                context.Estudiantes.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
